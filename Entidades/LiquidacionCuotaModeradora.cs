73209afcd8de26a8180f51e40af5587c7dbe7b6a
Estudiantes <Estudiantes@LAPTOP-KE5V54OR>
2023-09-20 11:01:33 -0500

    Agregar archivos de proyecto.

---
+++ Entidades/LiquidacionCuotaModeradora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class LiquidacionCuotaModeradora
+    {
+        public int NroLiquidacion { get; set; }
+        public int IdPaciente { get; set; }
+        public string TipoAfiliacion { get; set; }
+    }
+}
---
+++ Presentacion/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    public class Menu
+    {
+        public void MenuPrincipal()
+        {
+            int op;
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("IPS MAS SALUD Y VIDA");
+
+                Console.WriteLine();
+
+                Console.WriteLine("1. Registrar");
+
+                Console.WriteLine();
+
+                Console.WriteLine("2. Listado");
+
+                Console.WriteLine();
+
+                Console.WriteLine("3. Busqueda por afiliación");
+
+                Console.WriteLine();
+
+                Console.WriteLine("4. ");
+
+                Console.WriteLine();
+
+                Console.WriteLine("2. ");
+
+                Console.WriteLine();
+
+                Console.WriteLine("2. ");
+
+            } while (op != 7);
+        }
+    }
+}
