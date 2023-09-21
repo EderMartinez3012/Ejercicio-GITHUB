2061e1c9ed78c1c523137751e6023c08e11c29b9
JoseMojicaHernandez <89654121+josemojicahernandez@users.noreply.github.com>
2023-09-21 08:58:55 -0500

    Update LiquidacionCuotaModeradoraService.cs
    Compañero ya hice un fragmento de la capa lógica, estoy un poco enredado en los condicionales, mas tarde subo otro fragmento.
    nota: revise bien el repositorio.

--- Logica/LiquidacionCuotaModeradoraService.cs
+++ Logica/LiquidacionCuotaModeradoraService.cs
@@ -12,5 +12,83 @@ namespace Logica
         {
             return repositorio.ObtenerPersonasPorFiltro(filtro);
         }
+        
+        private LiquidacionCuotaModeradoraRepository repositorio = null;
+        private List<LiquidacionCuotaModeradora> liquidacionCuotaModeradoraList = null;
+        public LiquidacionCuotaModeradoraService()
+        {
+            repositorio = new LiquidacionCuotaModeradoraRepository();
+            liquidacionCuotaModeradoraList = repositorio.ConsultarTodoLiquidacion();
+
+        }
+        public string Guardar(LiquidacionCuotaModeradora liquidacionCuotaModeradora)
+        {
+            //validar
+            if (liquidacionCuotaModeradora == null)
+            {
+                return "no se puede guardar una persona sin informacion";
+            }
+
+            if (BuscarID(liquidacionCuotaModeradora.IdPaciente) != null)
+            {
+                return "liquidacion ya existente";
+            }
+            var msg = repositorio.Guardar(liquidacionCuotaModeradora);
+            liquidacionCuotaModeradoraList = repositorio.ConsultarTodoLiquidacion();
+            return msg;
+        }
+        public LiquidacionCuotaModeradora BuscarID(int id)
+        {
+            try
+            {
+                if (liquidacionCuotaModeradoraList == null)
+                {
+                    return null;
+                }
+                foreach (var item in liquidacionCuotaModeradoraList)
+                {
+                    if (id == item.IdPaciente)
+                    {
+                        return item;
+                    }
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+        public List<LiquidacionCuotaModeradora> ConsultarTodoLiquidacion()
+        {
+            return liquidacionCuotaModeradoraList;
+        }
+        public int Ganancias(object ganancia, int valoringreso, int valorgastos)
+        {
+            int Ganancias = valoringreso - valorgastos;
+            return Ganancias;
+        }
+        public void RegContributivo()
+        {
+            //Console.Write("Ingrese el Nro de salario del paciente");
+            //NroSalarios = int.Parse(Console.ReadLine());
+
+            //if (NroSalarios < 2)
+            //{
+            //    Tarifa = 0.15;
+            //}
+            //else if (NroSalarios >= 2 && <= 5)
+            //{
+            //    Tarifa = 0.20;
+            //}
+            //else
+            //{
+            //    Tarifa = 0.25;
+            //}
+
+            //CuotaModeladora = ValorServicio * Tarifa;
+        }
     }
+    
 }
